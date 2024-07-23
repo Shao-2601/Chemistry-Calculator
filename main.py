805110991f746dd9dfa7e4178cb8c9baab2b3ecb
Shao-2601 <sxleong@gmail.com>
2024-07-23 13:07:58 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+st.header('The Carolina Chemists\' Calculator')
+st.subheader('by Shao')
+st.write('Please select your desired calculation: ')
+
+button_1 = st.button('Acid-Base')
+button_2 = st.button('Dilutions')
+
+if button_2:
+    switch_page('Dilutions')
+
+if button_1:
+    switch_page('Acid-Base')
+
+
